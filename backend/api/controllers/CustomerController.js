21e61501f5cd141f37ae30e8a77535161966a354
Tan Choon Seng <eddietancs@gmail.com>
2020-09-25 14:43:53 +0800

    validation for customer validateform backend

--- backend/api/controllers/CustomerController.js
+++ backend/api/controllers/CustomerController.js
@@ -6,26 +6,41 @@ const CustomerController = () => {
   const register = async (req, res) => {
     const { body } = req;
 
-    if (body.password === body.password2) {
-      try {
-        const customer = await Customer.create({
-          customerName: body.customerName,
-          customerAge: body.customerAge,
-          serviceOfficerName: body.serviceOfficerName,
-          NRIC: body.NRIC,
-          registrationTime: body.registrationTime,
-          branchCode: body.branchCode,
-          productType: body.productType,
-        });
+    if(body.customerName == null 
+      || body.customerAge == null
+      || body.serviceOfficerName == null
+      || body.NRIC == null
+      || body.registrationTime == null
+      || body.branchCode == null
+      || body.productType == null){
+      return res.status(400).json({ msg: 'Bad Request: There are some empty Parameters' });
+    } 
 
-        return res.status(200).json({ id: customer.id });
-      } catch (err) {
-        console.log(err);
-        return res.status(500).json({ msg: 'Internal server error' });
-      }
+    if(body.customerName.length > 64 
+      || body.customerAge < 18
+      || body.serviceOfficerName.length > 64 
+      || body.NRIC.length != 9){
+      return res.status(400).json({ msg: 'Bad Request: There are bad Parameters' });
+    } 
+
+    try {
+      const customer = await Customer.create({
+        customerName: body.customerName,
+        customerAge: body.customerAge,
+        serviceOfficerName: body.serviceOfficerName,
+        NRIC: body.NRIC,
+        registrationTime: body.registrationTime,
+        branchCode: body.branchCode,
+        productType: body.productType,
+      });
+      
+      if(customer)
+
+      return res.status(200).json({ id: customer.id });
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({ msg: 'Internal server error' });
     }
-
-    return res.status(400).json({ msg: 'Bad Request: Passwords don\'t match' });
   };
 
   const getAll = async (req, res) => {
