a1406e3cfa3daa82e31065a27ee9cf91475908b0
Tan Choon Seng <eddietancs@gmail.com>
2020-09-25 13:19:23 +0800

    add configurations for db

---
+++ backend/config/routes/privateRoutes.js
@@ -0,0 +1,6 @@
+const privateRoutes = {
+  'GET /users': 'UserController.getAll',
+  'GET /customers': 'CustomerController.getAll',
+};
+
+module.exports = privateRoutes;
---
+++ backend/config/routes/publicRoutes.js
@@ -0,0 +1,9 @@
+const publicRoutes = {
+  'POST /user': 'UserController.register',
+  'POST /register': 'UserController.register', // alias for POST /user
+  'POST /login': 'UserController.login',
+  'POST /validateForm': 'CustomerController.register',
+  'GET /hello': 'HelloController.sayHello',
+};
+
+module.exports = publicRoutes;
